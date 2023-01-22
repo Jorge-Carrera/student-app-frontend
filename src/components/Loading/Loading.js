0c94c4b0e3154ce97150bccb133e9e360683d1c5
Jorge Carrera <JCarrera994@gmail.com>
2023-01-22 16:08:14 -0500

    adds spinner to loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,6 +3,8 @@ import './Loading.css'
 export default function Loading() {
   
   return (
-    <div className='Loading'>Loading...</div>
+    <div className='Loading'>Loading...
+    <div className='Loading--spinner'/>
+    </div>
   )
 }
